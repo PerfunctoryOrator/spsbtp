d242bead4e3c026b92df6ddf79e9b564670ed6f2
Yashdeep Singh Fauzdar <167981565+PerfunctoryOrator@users.noreply.github.com>
2024-06-02 14:38:13 +0530

    Add files via upload

---
+++ navbar/navbar.js
@@ -0,0 +1,67 @@
+document.write(`<header>
+        </header>
+        <nav class = "navbarContainer">
+            <div class = "navbar">
+                <a id = "home" title = "Homepage" href = "index.html"> Home </a>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "aboutUs" title = "All About Us"> About Us </button> 
+                    <div class = "dropdownContent">
+                        <a id = "aHistory" title = "" href = ""> Our History </a>
+                        <a id = "aInfrastructure" title = "" href = ""> Infrastructure </a>
+                        <a id = "aManagement" title = "" href = ""> Management Committee </a>
+                        <a id = "aFaculty" title = "" href = ""> Teaching Faculty </a>
+                        <a id = "aSelfDeclaration" title = "" href = ""> Self-Declaration </a>
+                    </div>
+                </div>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "admissions" title = ""> Admissions </button>
+                    <div class = "dropdownContent">
+                        <a id = "aAdmissionPolicy" title = "" href = ""> Admission Policy </a>
+                        <a id = "aFee"title = "" href = ""> Fee Structure </a>
+                        <a id = "aBooks" title = "" href = ""> Book List </a>
+                    </div>
+                </div>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "academics" title = ""> Academics </button>
+                    <div class = "dropdownContent">
+                        <a id = "aCalendar" title = "" href = ""> Annual Calendar </a>
+                        <a id = "aLibraryRules" title = "" href = ""> Library Rules </a>
+                        <a id = "aResults" title = "" href = ""> Results </a>
+                    </div>
+                </div>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "complianceSafety" title = ""> Compliance & Safety </button>
+                    <div class = "dropdownContent">
+                        <a id = "aSocietyCertificate" title = "" href = ""> Society Registration Certificate </a>
+                        <a id = "aNocState" title = "" href = ""> NOC from State Government </a>
+                        <a id = "aDeoCertificate" title = "" href = ""> DEO Certificate </a>
+                        <a id = "aBuildingSafety" title = "" href = ""> Building Safety Certificate </a>
+                        <a id = "aFireSafety" title = "" href = ""> Fire Safety Certificate </a>
+                        <a id = "aWaterSanitation" title = "" href = ""> Water and Sanitation </a>
+                        <a id = "aAffiliationExtension" title = "" href = ""> Extension of Affiliation </a>
+                    </div>
+                </div>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "studentLife" title = ""> Student Life </button>
+                    <div class = "dropdownContent">
+                        <a id = "aTimingsUniform" title = "" href = ""> Timings and Uniform </a>
+                        <a id = "aStudentGuidelines" title = "" href = ""> Guidelines for Students </a>
+                        <a id = "aPrayersAnthem" title = "" href = ""> Prayers and Anthem </a>
+                        <a id = "aHolidays" title = "" href = ""> Holiday List </a>
+                        <a id = "aParentRecommendations" title = "" href = ""> Recommendations to Parents </a>
+                        <a id = "aPTA"title = "" href = ""> Parent-Teacher Association </a>
+                        <a id = "aCabinet" title = "" href = ""> Cabinet </a>
+                        <a id = "aInternalComplaints" title = "" href = ""> Internal Complaints Committee </a>
+                    </div>
+                </div>
+                <div class = "dropdownContainer">
+                    <button class = "dropdownButton" id = "newsEvents" title = ""> News & Events </button>
+                    <div class = "dropdownContent">
+                        <a id = "aNews" title = "" href = ""> News </a>
+                        <a id = "aAnnualReports" title = "" href = ""> Annual Reports </a>
+                        <a id = "aGallery" title = "" href = ""> Gallery </a>
+                    </div>
+                </div>
+                <a id = "mandatoryDisclosure" title = "" href = "" style = "float: right;"> Mandatory Public Disclosure </a>
+            </div>
+        </nav>`)
